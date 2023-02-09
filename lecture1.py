a4e52cc9030a5a32cb23d64b11cab3ba45aa3c00
«Maks» <the7usinessman23@gmail.com>
2023-02-09 06:51:04 +0500

    Save

---
+++ lecture1.py
@@ -0,0 +1,21 @@
+# # a = int(input('Введите первое число: '))
+# # b = int(input('Введите второе число: '))
+
+# # print(a, ' + ', b, ' = ', a + b)
+
+# a = 1 > 4
+# print(a)
+
+line = ""
+for i in range(3):
+    line = ""
+    for j in range(2):
+        line += "*"
+    print(line)
+
+
+
+
+
+
+
