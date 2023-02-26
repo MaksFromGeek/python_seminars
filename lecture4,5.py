77b6acb141cb0629a38cf53e85e347dba3c5b0fe
«Maks» <the7usinessman23@gmail.com>
2023-02-26 19:08:59 +0500

    Save

--- lecture4,5.py
+++ lecture4,5.py
@@ -9,13 +9,14 @@
 
 
 # Задача 1
-list_1 = [1,2,3,5,8,15,23,38]
-list_1 = [(i, i*i) for i in list_1 if i % 2 == 0]
-print(list_1)
+# list_1 = [1,2,3,5,8,15,23,38]
+# list_1 = [(i, i*i) for i in list_1 if i % 2 == 0]
+# print(list_1)
 
+import os
+print(os.getcwd())
 
-
-
+# /Users/maksimpolanin/Desktop/dotnet/python_course/python_seminars
 
 
 
